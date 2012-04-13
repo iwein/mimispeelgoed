62ce2680316d81ad12d534b0671b3b7a5a6f05f0
Iwein Fuld <iwein@fuld.net>
2012-04-13 11:59:39 +0200

    Add fixed testcase, now ready for back-end integration.

---
+++ src/test/scala/com/example/ServiceSpec.scala
@@ -0,0 +1,30 @@
+package com.example
+
+import org.specs2.mutable._
+import cc.spray._
+import test._
+import http._
+import HttpMethods._
+import StatusCodes._
+
+class ServiceSpec extends Specification with SprayTest with MimiService {
+  
+  "The MimiService" should {
+    "return a greeting for GET requests to the root path" in {
+      testService(HttpRequest(GET, "/")) {
+        staticResourceService
+      }.response.content.as[String].right.getOrElse("Failed") must contain ("MimiSpeelgoed")
+    }
+    "leave GET requests to other paths unhandled" in {
+      testService(HttpRequest(GET, "/kermit")) {
+        staticResourceService
+      }.handled must beFalse
+    }
+    "return a MethodNotAllowed error for POST requests to the root path" in {
+      testService(HttpRequest(POST, "/")) {
+        staticResourceService
+      }.response mustEqual HttpResponse(MethodNotAllowed, "HTTP method not allowed, supported methods: GET")
+    }
+  }
+  
+}
