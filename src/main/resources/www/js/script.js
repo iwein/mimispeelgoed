49f9403c31f9bfa99940a036a5336c0a5c2a7998
Iwein Fuld <iwein@fuld.net>
2012-04-14 16:24:32 +0200

    Add client side rendering and responsive design

--- src/main/resources/www/js/script.js
+++ src/main/resources/www/js/script.js
@@ -1,5 +1,3 @@
-
-
 var checkBoxTemplate = '<div class="control-group">\
                         <label class="span2">{{label}}</label>\
                         <input type="checkbox" class="span3">\
@@ -10,7 +8,18 @@ $(document).ready(function() {
   $.ajax(
         { url:"templates/edit-item.html",
           type: "GET",
-          success: function(result) {
+          success: function(template) {
+            $.ajax(
+                  { url:"products",
+                    type: "GET",
+                    success: function(json) {
+                      var data = new Object();
+                      data.products = eval(json);
+                      console.log(data);
+                      var elem = $(Mustache.render(template, data));
+                      $("#product-container").html(elem);
+                    }
+                  });
           }
         });
 });
