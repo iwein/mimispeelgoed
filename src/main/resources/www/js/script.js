bc1ebc349a9c8fa5d6fe45d9e629509927a75abc
Albert Sikkema <albert.sikkema@uppenergy.com>
2012-06-01 00:00:02 +0200

    Refactoring of Router, initialization and views
    Changed ResourceLoader to 'call' the function with the context
    Added views for all the routes
    Changed rendering of views the this.el
    Added products Collection and Model
    Changed initialization order (and moved it a bit around)
    Rendering of products when the reset event is triggered.
    Cleanup of old views when they are discarded

--- src/main/resources/www/js/script.js
+++ src/main/resources/www/js/script.js
@@ -1,187 +1,216 @@
 var Tags = (function (Backbone) {
-  var TagsModel = Backbone.Model.extend({
-    defaults: {
-      "tags" : []
-    },
-    url: "tags"
+    var TagsModel = Backbone.Model.extend({
+        defaults:{
+            "tags":[]
+        },
+        url:"tags"
 
-  });
+    });
 
-  var TagsView = Backbone.View.extend({
-    el: "#tags-filter",
+    var TagsView = Backbone.View.extend({
+        el:"#tags-filter",
 
-    render: function() {
-      ResourceCache.doWithResource("templates/tags.html", function(cache, context){
-        $(context.el).html($(Mustache.render(cache["templates/tags.html"], _.clone(context.model.attributes))));
-      } , this);
-    },
+        render:function () {
+            ResourceCache.doWithResource("templates/tags.html", function (cache) {
+                $(this.el).html($(Mustache.render(cache["templates/tags.html"], (this.model.toJSON()))));
+            }, this);
+            return this;
+        },
 
-    events: {
-      "click .label" : "toggleActiveLabel",
-      "click button#all-labels": "selectAllLabels",
-      "click button#no-labels": "selectNoLabels"
-    },
+        events:{
+            "click .label":"toggleActiveLabel",
+            "click button#all-labels":"selectAllLabels",
+            "click button#no-labels":"selectNoLabels"
+        },
 
-    toggleActiveLabel: function(event) {
-      $(event.currentTarget).toggleClass("label-info active-tag");
-      showProductsForActiveTags();
-    },
+        toggleActiveLabel:function (event) {
+            $(event.currentTarget).toggleClass("label-info active-tag");
+            showProductsForActiveTags();
+        },
 
-    selectAllLabels: function(event) {
-      //
-    },
+        selectAllLabels:function (event) {
+            //
+        },
 
-    selectNoLabels: function(event) {
-      //
+        selectNoLabels:function (event) {
+            //
+        }
+    });
+
+    var tagsModel = new TagsModel();
+
+    var tagsView = new TagsView({model:tagsModel});
+
+
+    var showProductsForActiveTags = function () {
+        var activeTags = [];
+        $("#tags-filter .active-tag").each(function () {
+            activeTags.push($(this).text());
+        });
+
+        $(".product").each(function () {
+            var self = this;
+            var classes = $(self).attr("tags").trim().split(" ");
+            if ($.grep(classes,
+                function (elem) {
+                    return $.inArray(elem, activeTags) > -1
+                }).length > 0) {
+                $(self).show();
+            } else {
+                $(self).hide();
+            }
+        });
+    };
+
+    tagsModel.on("change", function () {
+        tagsView.render();
+    });
+
+    return {
+        View:tagsView,
+        Model:tagsModel
     }
-  });
-
-  var tagsModel = new TagsModel();
-
-  var tagsView = new TagsView({model:tagsModel});
-
-
-  var showProductsForActiveTags = function() {
-    var activeTags = [];
-    $("#tags-filter .active-tag").each(function() {
-      activeTags.push($(this).text());
-    });
-
-    $(".product").each(function() {
-      var self = this;
-      var classes = $(self).attr("tags").trim().split(" ");
-      if ($.grep(classes,
-            function(elem) {
-              return $.inArray(elem, activeTags) > -1
-            }).length > 0) {
-        $(self).show();
-      } else {
-        $(self).hide();
-      }
-    });
-  };
-
-  tagsModel.on("change", function() {
-    tagsView.render();
-  });
-
-  return {
-    View: tagsView,
-    Model: tagsModel
-  }
 }(Backbone));
 
-var ResourceCache = (function() {
-  var cache = {};
 
-  return {
-    loadResources:function(urls) {
-      _.each(urls, function(url){
-        if (!cache[url]) $.get(url);
-      });
-    },
+var ProductsModule = (function (Backbone) {
+    var Product = Backbone.Model.extend({
+    });
 
-    doWithResource:function(url, callback, context) {
-     var resource = cache[url];
-     if (!resource) {
-       $.get(url, function(data){
-         cache[url] = data;
-         callback(cache, context)
-       })
-     } else {
-       callback(cache, context);
-     }
-    },
+    var Products = Backbone.Collection.extend({
+        url:'/products',
+        model:Product
+    });
 
-    doWithResources:function(urls, callback, context) {
-      var self = this;
-
-      var doIt = _.after(urls.length, callback);
-      _.each(urls, function(url){
-        self.doWithResource(url, doIt, context);
-      })
+    return {
+        Collection:Products,
+        Model:Product
     }
-  };
+}(Backbone));
+
+
+var ResourceCache = (function () {
+    var cache = {};
+
+    return {
+        loadResources:function (urls) {
+            _.each(urls, function (url) {
+                if (!cache[url]) $.get(url);
+            });
+        },
+
+        doWithResource:function (url, callback, context) {
+            var resource = cache[url];
+            if (!resource) {
+                $.get(url, function (data) {
+                    cache[url] = data;
+                    callback.call(context, cache)
+                })
+            } else {
+                callback.call(context, cache);
+            }
+        },
+
+        doWithResources:function (urls, callback, context) {
+            var self = this;
+
+            var doIt = _.after(urls.length, callback);
+            _.each(urls, function (url) {
+                self.doWithResource(url, doIt, context);
+            })
+        }
+    };
 
 })();
 
-var App = (function(Backbone){
-
-  var Product = Backbone.Model.extend({});
-
-  var Products = Backbone.Collection.extend({
-    url: '/products',
-    model: Product
-  });
-
-
-  var MainRouter = Backbone.Router.extend({
-    routes: {
-      "":"renderHome",
-      "about":"renderAbout",
-      "contact":"renderContact"
-    },
-    renderHome:function() {
-      console.log("Render Home - Fetching products.html");
-      ResourceCache.doWithResources(["templates/product.html", "products"], function(cache){
-        var products = {products: eval(cache["products"])};
-        var template = cache["templates/product.html"];
-        var elem = $(Mustache.render(template, products));
-        $("#content-container").html(elem);
-        $("#product-controls").show();
-      });
-    },
-
-    renderProducts: function() {
-      console.log('render products here...');
-    },
-
-    renderAbout:function() {
-      console.log("Fetching about.html");
-      $.ajax(
-            { url:"templates/about.html",
-              type: "GET",
-              success: function(aboutTemplate) {
-                console.log("Rendering about");
-                var elem = $(Mustache.render(aboutTemplate));
-                $("#content-container").html(elem);
-                $("#product-controls").hide();
-              }
-            });
-    },
-    renderContact:function() {
-      console.log("Fetching contact.html");
-      $.ajax(
-            { url:"templates/contact.html",
-              type: "GET",
-              success: function(aboutTemplate) {
-                console.log("Rendering contact");
-                var elem = $(Mustache.render(aboutTemplate));
-                $("#content-container").html(elem);
-                $("#product-controls").hide();
-              }
-            });
+var App = (function (Backbone) {
+    function renderInContentContainer(view) {
+        if (this.containerView) {
+            this.containerView.remove();
+        }
+        this.containerView = view;
+        $("#content-container").html(view.render().el);
     }
-  });
-  return {
-    Router: new MainRouter()
-  }
+
+
+    var MainRouter = Backbone.Router.extend({
+        routes:{
+            "":"renderHome",
+            "about":"renderAbout",
+            "contact":"renderContact"
+        },
+
+        renderHome:function () {
+            this.products = new ProductsModule.Collection();
+            this.products.bind('reset', this.renderProducts, this);
+            this.products.fetch();
+        },
+
+        renderProducts:function () {
+            $("#product-controls").show();
+            renderInContentContainer(new ProductsView({model:this.products}));
+        },
+
+        renderAbout:function () {
+            $("#product-controls").hide();
+            renderInContentContainer(new AboutView());
+        },
+
+        renderContact:function () {
+            $("#product-controls").hide();
+            renderInContentContainer(new ContactView());
+        }
+    });
+
+    var ProductsView = Backbone.View.extend({
+        render:function () {
+            ResourceCache.doWithResources(["templates/product.html"], function (cache) {
+                var template = cache["templates/product.html"];
+                $(this.el).html($(Mustache.render(template, {products:this.model.toJSON()})));
+            }, this);
+            return this;
+        }
+    });
+
+    var AboutView = Backbone.View.extend({
+        render:function () {
+            ResourceCache.doWithResources(["templates/about.html"], function (cache) {
+                $(this.el).html($(Mustache.render(cache["templates/about.html"])));
+            }, this);
+            return this;
+        }
+    });
+
+    var ContactView = Backbone.View.extend({
+        render:function () {
+            ResourceCache.doWithResources(["templates/contact.html"], function (cache) {
+                $(this.el).html($(Mustache.render(cache["templates/contact.html"])));
+            }, this);
+            return this;
+        }
+    });
+
+    return {
+        initialize:function () {
+            ResourceCache.loadResources(["templates/tags.html",
+                "templates/product.html",
+                "templates/contact.html",
+                "templates/about.html"
+            ]);
+
+            ResourceCache.doWithResource("templates/tags.html", function (tagsTemplate) {
+                Tags.Model.fetch();
+            });
+            new MainRouter();
+            Backbone.history.start();
+        }
+    }
 }(Backbone));
 
 var templates = new Object();
 
-
-$(document).ready(function() {
-  ResourceCache.loadResources(["templates/tags.html",
-    "templates/product.html",
-    "templates/contact.html",
-    "templates/about.html"
-  ]);
-
-  Backbone.history.start();
-  ResourceCache.doWithResource("templates/tags.html", function(tagsTemplate) {
-    Tags.Model.fetch();
-  });
+$(document).ready(function () {
+    App.initialize();
 });
 
 
